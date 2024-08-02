a6e6af644b9f9cc436172c9caa8f49dc84434b96
sumin <ansumin0103@gmail.com>
2024-08-02 17:03:10 +0900

    catching bugs

--- src/intro.js
+++ src/intro.js
@@ -10,36 +10,36 @@ export default class IntroScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(this.game.scale.width / 2, 200, 'Web Game', {
+        this.add.text(this.game.scale.width / 2, 200, 'catching bugs', {
             fontFamily: 'Arial',
             fontSize: 80,
             color: '#87CEFA'
         }).setOrigin(0.5, 0.5)
 
-        const button = this.add.text(this.game.scale.width, 500, 'Game Start', {
-            fontFamily: 'Arial',
-            fontSize:50,
-            color: 'white'
-        }).setInteractive()
-        .on('pointerdown', () => this.scene.start('Main'));
-
-        // this.textTap = this.add.text(this.game.scale.width/2, 500, 'Tap to continue', {
+        // const button = this.add.text(this.game.scale.width, 500, 'Game Start', {
         //     fontFamily: 'Arial',
         //     fontSize:50,
         //     color: 'white'
-        // }).setAlpha(1).setOrigin(0.5, 0.5)
+        // }).setInteractive()
+        // .on('pointerdown', () => this.scene.start('Main'));
 
-        // this.input.on('pointerdown', (point) => {
-        //     this.scene.start('Main') //Main 씬으로 이동
-        // })
+        this.textTap = this.add.text(this.game.scale.width/2, 500, 'Tap to continue', {
+            fontFamily: 'Arial',
+            fontSize:50,
+            color: 'white'
+        }).setAlpha(1).setOrigin(0.5, 0.5)
 
-        this.tweens.add({
-            targets: this.textTap,
-            alpha: 0,
-            duration: 500,
-            ease: 'Linear',
-            yoyo: true,
-            repeat: -1
+        this.input.on('pointerdown', (point) => {
+            this.scene.start('Main') //Main 씬으로 이동
+        })
+
+        this.tweens.add({    // 애니메이션 설정 -> 텍스트가 0.5초간 투명 -> 불투명을 반복
+            targets: this.textTap,  // 애니메이션을 적용할 대상
+            alpha: 0,        // alpha 값을 0으로 변경하여 투명하게 만듦
+            duration: 500,   // 애니메이션의 지속 시간 (밀리초)
+            ease: 'Linear',  // 애니메이션의 보간 방법 (선형 보간)
+            yoyo: true,      // 애니메이션이 끝나면 반대 방향으로 재생
+            repeat: -1       // 애니메이션을 무한히 반복
         })
     }
 }
--- src/main.js
+++ src/main.js
@@ -7,16 +7,59 @@ export default class MainScene extends Phaser.Scene {
 
     preload() {
         this.load.image('mouse', '../asset/image/mouse1.png');
+        this.load.image('bug', '../asset/image/bug.png')
     }
 
     create() {
+        // phaser는 객체 추가 순서대로 z-index(레이어 순서)가 결정되기 때문에 bug를 먼저 생성하고 마우스를 생성하도록 수정 -> 근데 안 됨
+        this.time.addEvent({
+            delay: 1000, // 1초마다 실행
+            callback: this.addRandomBug,
+            callbackScope: this,
+            loop: true
+        });
+
         this.input.setDefaultCursor('none'); //마우스 포인터 숨기기
         this.mouseImage = this.add.image(this.scale.width / 2, this.scale.height / 2, 'mouse');
+        this.mouseImage.setDepth(1); //마우스 이미지의 깊이를 1로 설정해 다른 이미지보다 앞에 있도록 함
+
+        // this.bugImage = this.add.image(this.scale.width / 2, this.scale.height / 2, 'bug');
     }
 
     update(){
         //이미지의 위치를 마우스 포인터 위치로 할당
         this.mouseImage.x = this.input.activePointer.x;
         this.mouseImage.y = this.input.activePointer.y;
+    }
+
+    addRandomBug() {
+        const { width, height } = this.scale;
+
+        // 랜덤 위치 생성
+        const x = Phaser.Math.Between(0, width);
+        const y = Phaser.Math.Between(0, height);
+
+        // 랜덤 크기 생성
+        const scale = Phaser.Math.FloatBetween(0.05, 0.4);
+
+        // 새로운 버그 이미지 추가
+        const bug = this.add.image(x, y, 'bug');
+        bug.setScale(scale);
+        bug.setDepth(0);
+        bug.setInteractive(); // 버그 이미지에 대해 인터랙티브 설정
+
+        // 클릭 이벤트 추가
+        bug.on('pointerdown', () => {
+            console.log('Bug clicked!');
+            bug.destroy(); // 클릭 시 버그 이미지 삭제
+        });
+
+        this.time.addEvent({
+            delay: 1000, // 3초 후에 실행
+            callback: () => {
+                bug.destroy();
+            },
+            callbackScope: this
+        });
     }
 }
